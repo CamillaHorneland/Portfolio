b533ff789de3b27643d4145e2f445bf90ad16dec
Camilla <104870685+CamillaHorneland@users.noreply.github.com>
2024-03-05 00:50:34 +0100

    set up files and folders. Package.json file working after restart of vscode

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
-export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      test
-    </h1>
-  )
+import {  Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import ContactPage from "./pages/ContactPage";
+import ProjectsPage from "./pages/ProjectsPage";
+import NotFoundPage from "./pages/NotFoundPage";
+// import Layout from "./components/layout/Layout";
+
+
+function App() {
+    return (
+   
+        <Routes>
+          {/* <Route path="/" element={<Layout />}> */}
+            <Route index element={<HomePage />} />
+            <Route path="contact" element={<ContactPage />} />
+             <Route path="projects" element={<ProjectsPage />} />
+            <Route path="*" element={<NotFoundPage />} />
+          {/* </Route> */}
+        </Routes>
+    
+  );
 }
+
+export default App;
