05cc912e255214615ceb8ca11c27e44084cacb83
Camilla <104870685+CamillaHorneland@users.noreply.github.com>
2024-05-24 23:17:01 +0200

    active menu changed to pink

--- src/components/layout/nav/Nav.jsx
+++ src/components/layout/nav/Nav.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import { HiMenuAlt2 } from 'react-icons/hi';
 
 const Nav = () => {
+  const location = useLocation();
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
   const handleToggleMobileMenu = () => {
@@ -10,39 +11,35 @@ const Nav = () => {
   };
 
   return (
-      <nav className="sm:flex sm:items-center sm:justify-between bg-white z-60 p-4">
+    <nav className="sm:flex sm:items-center sm:justify-between bg-white z-60 p-4">
       <HiMenuAlt2
         className="text-6xl cursor-pointer sm:hidden text-pink-300"
         onClick={handleToggleMobileMenu}
       />
       <ul className={`${isMobileMenuOpen ? 'block' : 'hidden'} sm:flex sm:space-x-4 text-gray-800 ml-auto mt-10 mr-4`}>
-        <li className="m-4 group">
+        <li className={`m-4 group ${location.pathname === '/' ? 'active' : ''}`}>
           <NavLink
             to="/"
-            className="active mx-4 transition-all hover:text-lg font-bold text-gray-400"
+            className={`mx-4 transition-all hover:text-lg font-bold text-gray-400 ${location.pathname === '/' ? 'text-pink-300' : ''}`}
             onClick={() => setIsMobileMenuOpen(false)}
           >
             Home
           </NavLink>
         </li>
-        <li className="m-4 group">
+        <li className={`m-4 group ${location.pathname === '/projects' ? 'active' : ''}`}>
           <NavLink
             to="/projects"
-            className={`active mx-4 transition-all hover:text-lg font-bold text-gray-400 ${isMobileMenuOpen ? 'bg-white' : ''}`}
-            onClick={() => {
-              setIsMobileMenuOpen(false);
-            }}
+            className={`mx-4 transition-all hover:text-lg font-bold text-gray-400 ${location.pathname === '/projects' ? 'text-pink-300' : ''}`}
+            onClick={() => setIsMobileMenuOpen(false)}
           >
             Projects
           </NavLink>
         </li>
-        <li className="m-4 group">
+        <li className={`m-4 group ${location.pathname === '/contact' ? 'active' : ''}`}>
           <NavLink
             to="/contact"
-            className="active mx-4 transition-all hover:text-lg font-bold text-gray-400"
-            onClick={() => {
-              setIsMobileMenuOpen(false);
-            }}
+            className={`mx-4 transition-all hover:text-lg font-bold text-gray-400 ${location.pathname === '/contact' ? 'text-pink-300' : ''}`}
+            onClick={() => setIsMobileMenuOpen(false)}
           >
             Contact
           </NavLink>
@@ -53,6 +50,8 @@ const Nav = () => {
 };
 
 export default Nav;
+
+
 
 
 
